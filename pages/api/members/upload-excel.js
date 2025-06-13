c79b487b871b9b1d5f2c0303189360da4c2b1cbc
Autophile-Dev <waleed.zaheer55@gmail.com>
2025-06-13 18:16:15 +0800

    Remove method validation

--- pages/api/members/upload-excel.js
+++ pages/api/members/upload-excel.js
@@ -19,9 +19,7 @@ function parseDate(value) {
 }
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+ 
 
   await connectDB();
 
